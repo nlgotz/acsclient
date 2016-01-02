f9419f28489e4947352fdb751597bb6bace1af20
Nathan Gotz <nlgotz+github@gotzweb.com>
2016-01-02 11:10:46 -0600

    Published to PyPi

--- setup.py
+++ setup.py
@@ -12,16 +12,19 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='acsclient',
-    version='1.0.0',
+    version='1.0.3',
     description="Access Cisco ACS 5.6 API",
     classifiers=[],
-    keywords='cisco acs access control',
+    keywords='cisco acs access control acsclient',
     author='Nathan Gotz',
     author_email='nathan@gotz.co',
     url='https://github.com/nlgotz/acsclient',
     license='Apache 2.0',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
+    package_data={
+        'acsclient':['templates/*.j2'],
+    },
     zip_safe=False,
     install_requires=[
         'requests',
