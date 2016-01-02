f465758b0448e03010592247e6d03ff7deed48a4
Nathan Gotz <nlgotz+github@gotzweb.com>
2016-01-02 11:12:12 -0600

    Updated for Pypi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='acsclient',
-    version='1.0.3',
+    version='1.0.4',
     description="Access Cisco ACS 5.6 API",
     classifiers=[],
     keywords='cisco acs access control acsclient',
